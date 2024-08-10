c6ae0f59bf2f5bef9b3685b49a87c66ca1f80e17
Nandanuness <fernanda.nunes@edu.unifil.br>
2024-08-09 21:57:32 -0300

    feat criação da requisição HTTP com API

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ app.get('/', (req,res) =>{
     res.send('Hello world');
 });
 
+const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
 app.get('/rota', (req, res) => {
     res.send('Outra rota');
 });
 
 app.get('/consulta-cep/:cep', async (req, res) => {
     const cep = req.params.cep;
+    if (cepRegex.test(cep)){
+        return res.status(400).send('CEP Inválido');
+    }
 
     try{
         const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
